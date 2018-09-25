64902d73cb60c181950e1322b60fcf54f3ccb9b3
vincentballet <vincent.ballet@me.com>
2018-09-25 09:30:01 +0200

    Adds template files

---
+++ da18_project_template/da_proc.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <time.h>
+
+static int wait_for_start = 1;
+
+static void start(int signum) {
+	wait_for_start = 0;
+}
+
+static void stop(int signum) {
+	//reset signal handlers to default
+	signal(SIGTERM, SIG_DFL);
+	signal(SIGINT, SIG_DFL);
+
+	//immediately stop network packet processing
+	printf("Immediately stopping network packet processing.\n");
+
+	//write/flush output file if necessary
+	printf("Writing output.\n");
+
+	//exit directly from signal handler
+	exit(0);
+}
+
+int main(int argc, char** argv) {
+
+	//set signal handlers
+	signal(SIGUSR1, start);
+	signal(SIGTERM, stop);
+	signal(SIGINT, stop);
+
+
+	//parse arguments, including membership
+	//initialize application
+	//start listening for incoming UDP packets
+	printf("Initializing.\n");
+
+
+	//wait until start signal
+	while(wait_for_start) {
+		struct timespec sleep_time;
+		sleep_time.tv_sec = 0;
+		sleep_time.tv_nsec = 1000;
+		nanosleep(&sleep_time, NULL);
+	}
+
+
+	//broadcast messages
+	printf("Broadcasting messages.\n");
+
+
+	//wait until stopped
+	while(1) {
+		struct timespec sleep_time;
+		sleep_time.tv_sec = 1;
+		sleep_time.tv_nsec = 0;
+		nanosleep(&sleep_time, NULL);
+	}
+}
