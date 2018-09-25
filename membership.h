dae9be39e239705a3f826ea7683c5e92dfc6464b
Sergei Volodin <sergei.volodin@epfl.ch>
2018-09-25 09:23:39 +0200

    Add comments

--- membership.h
+++ membership.h
@@ -1,3 +1,9 @@
+/** @file membership.h
+ * @brief Parser for membership file
+ * @author Volodin Sergei, EPFL
+ * @date Sep 25, 2018
+ */
+
 #ifndef MEMBERSHIP_H
 #define MEMBERSHIP_H
 
@@ -9,6 +15,7 @@ using std::string;
 using std::ifstream;
 using std::map;
 
+/** @brief Parser for membership file */
 class Membership
 {
 private:
@@ -16,8 +23,24 @@ private:
     map<int, string> ips;
     map<int, int> ports;
 public:
+    /**
+     * @brief Load membership file
+     * @param File path
+     */
     Membership(string file);
+
+    /**
+     * @brief Get IP of a process
+     * @param process to get IP of
+     * @return IP as a string
+     */
     string getIP(int process);
+
+    /**
+     * @brief Get port of a process
+     * @param process to get port of
+     * @return Port as an int
+     */
     int getPort(int process);
 };
 
