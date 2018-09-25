31cb10e5018552965d779442d4fb8b12ac2e53f9
Sergei Volodin <sergei.volodin@epfl.ch>
2018-09-25 09:05:38 +0200

    Add comments

--- main.cpp
+++ main.cpp
@@ -1,3 +1,9 @@
+/** @file main.cpp
+ * @brief Main file for the Distributed Computing project
+ * @date Sep 25 2018
+ * @author Volodin Sergei, EPFL
+ */
+
 #include <iostream>
 #include <string>
 #include <stdlib.h>
@@ -7,6 +13,10 @@ using std::string;
 using std::cout;
 using std::endl;
 
+/**
+ * @brief Handle the SIGUSR1 signal
+ * @param signal_num must be SIGUSR1
+ */
 void onSignalUsr1(int signal_num)
 {
     if(signal_num != SIGUSR1) return;
@@ -14,36 +24,61 @@ void onSignalUsr1(int signal_num)
     // Start broadcasting messages
 }
 
+/**
+ * @brief Write current state and exit on application crash (SIGINT/SIGTERM)
+ */
 void writeOutputAndHalt()
 {
     // writing output file
     exit(0);
 }
 
+/**
+ * @brief Handle the SIGINT signal
+ * @param signal_num must be SIGINT
+ */
 void onSignalInt(int signal_num)
 {
     if(signal_num != SIGINT) return;
     writeOutputAndHalt();
 }
 
+
+/**
+ * @brief Handle the SIGTERM signal
+ * @param signal_num must be SIGTERM
+ */
 void onSignalTerm(int signal_num)
 {
     if(signal_num != SIGTERM) return;
     writeOutputAndHalt();
 }
 
+/**
+ * @brief main() function for the project
+ * @param argc Number of arguments
+ * @param argv Array of parameters
+ * @return
+ */
 int main(int argc, char** argv)
 {
+    // map signals to their handlers
     signal(SIGTERM, onSignalTerm);
     signal(SIGINT, onSignalInt);
     signal(SIGUSR1, onSignalUsr1);
 
+    // argument processing
     argc--;
+
+    // not enough arguments, show usage
     if(argc < 2)
     {
         cout << "Usage: " << argv[0] << " n membership [extra params...]" << endl;
+        return 1;
     }
-    else if(argc == 2)
+
+    // obtaining n and membership file
+    else
     {
         int n = atoi(argv[1]);
         string membership = argv[2];
