984feeb2cd9f19d6114c9c4f97717b93f7cb57a5
Sergei Volodin <sergei.volodin@epfl.ch>
2018-10-20 17:07:32 +0200

    Check beb broadcast

--- tests/test_fifo_all_properties.py
+++ tests/test_fifo_all_properties.py
@@ -28,23 +28,6 @@ for key, value in logs.items():
 # how many messages should have been sent by each process?
 expected_messages = 10
 
-# messages broadcast by a process. idx -> array
-broadcast_by = {i: [] for i in range(1, n + 1)}
-
-# messages delivered by a process. (idx, idx) -> array
-delivered_by_from = {(i, j): [] for i in range(1, n + 1) for j in range(1, n + 1)}
-
-# Filling in broadcast_by and delivered_by
-for process in range(1, n + 1):
-  for entry in logs[process]:
-    if entry.startswith("b "):
-      broadcast_by[process] += [int(entry[2:])]
-    elif entry.startswith("d "):
-      by = process
-      from_ = int(entry.split()[1])
-      content = int(entry.split()[2])
-      delivered_by_from[(by, from_)] += [content]
-
 # Were there errors?
 were_errors = False
 
@@ -55,14 +38,51 @@ def soft_assert(condition, message):
         print("ASSERT failed " + message)
         were_errors = True
 
-# Checking that each process has sent its messages
-for process in range(1, n + 1):
-  soft_assert(broadcast_by[process] == range(expected_messages), "Process %d should send all messages" % process)
+def get_send_recv(prefix):
+  """ Return pair (broadcast_by, delivered_by_from) for a prefix """
+  # messages broadcast by a process. idx -> array
+  broadcast_by = {i: [] for i in range(1, n + 1)}
 
-# Checking if messages are correct
-for dst in range(1, n + 1):
-  for src in range(1, n + 1):
-    soft_assert(delivered_by_from[(dst, src)] == range(expected_messages), "Process %d should receive all messages in correct order from %d; Got array %s" % (dst, src, str(delivered_by_from[(dst, src)])))
+  # messages delivered by a process. (idx, idx) -> array
+  delivered_by_from = {(i, j): [] for i in range(1, n + 1) for j in range(1, n + 1)}
+
+  # Filling in broadcast_by and delivered_by
+  for process in range(1, n + 1):
+    for entry in logs[process]:
+      if entry.startswith(prefix + "b "):
+        broadcast_by[process] += [int(entry[2 + len(prefix):])]
+      elif entry.startswith(prefix + "d "):
+        by = process
+        from_ = int(entry.split()[1])
+        content = int(entry.split()[2])
+        delivered_by_from[(by, from_)] += [content]
+
+  return broadcast_by, delivered_by_from
+
+def check_send_recv(prefix):
+  """ Check that all messages were sent and received in correct order """
+  # obtain messages
+  broadcast_by, delivered_by_from = get_send_recv(prefix)
+
+  # Checking that each process has sent its messages
+  for process in range(1, n + 1):
+    soft_assert(broadcast_by[process] == range(expected_messages), "Process %d should send all messages; Prefix %s" % (process, prefix))
+
+  # Checking if messages are correct
+  for dst in range(1, n + 1):
+    for src in range(1, n + 1):
+      A = delivered_by_from[(dst, src)]
+      B = range(expected_messages)
+      if prefix == "beb": # not care about repetitions/order
+        A = set(A)
+        B = set(B)
+      soft_assert(A == B, "Process %d should receive all messages in correct order from %d; Got array %s; Prefix %s" % (dst, src, str(delivered_by_from[(dst, src)]), prefix))
+
+# check FIFO
+check_send_recv("")
+
+# check BestEffortBroadcast
+check_send_recv("beb")
 
 # printing the last line with status
 print("INCORRECT" if were_errors else "CORRECT")
