c24e0e729629553bea392b838b0a20a802442eb9
Sergei Volodin <sergei.volodin@epfl.ch>
2018-10-31 14:28:12 +0100

    Add ACK from self

--- tests/run_with_crashes.py
+++ tests/run_with_crashes.py
@@ -8,6 +8,9 @@ if len(sys.argv) < 2:
     print(" The directory must contain the membership file and also the da_proc executable")
     sys.exit(0)
 
+# messages to send
+m = 100
+
 # time to initialize
 wait_time = 5
 
@@ -16,6 +19,12 @@ max_crash_time_ms = 50
 
 # maximal time to send stuff
 max_send_time_ms = 5000
+
+# maximal pause time
+max_pause_time_ms = 600
+
+# process is stopped with this probability at each millisecond
+p_stopped = 0.01
 
 # Obtaining directory name
 d = sys.argv[1]
@@ -33,6 +42,9 @@ print('There are %d processes' % n)
 # maximal number of crashed processes
 max_crashed = n // 2 if len(sys.argv) == 2 else 0
 
+# checking that max_crashed is valid
+assert 2 * max_crashed < n, "Must have a valid majority"
+
 print("Crashing %d at most" % max_crashed)
 
 # choosing how many processes to crash
@@ -49,6 +61,9 @@ crash_times = list(map(int, np.random.rand(n_crashed) * max_crash_time_ms))
 
 print("Crashing %d processes: %s at times %s" % (n_crashed, crashed_processes, crash_times))
 
+# process is stopped for this time at each millisecond
+get_t_stopped = lambda : np.random.rand() * max_pause_time_ms
+
 # writing down crashed processes
 open('crashed.log', 'w').write(' '.join(map(lambda x : str(x + 1), crashed_processes)))
 
@@ -60,7 +75,7 @@ os.system("killall -9 da_proc")
 
 # creating processes
 for i in range(n):
-  pids += [os.spawnlp(os.P_NOWAIT, './da_proc', 'da_proc', str(i + 1), 'membership')]
+  pids += [os.spawnlp(os.P_NOWAIT, './da_proc', 'da_proc', str(i + 1), 'membership', str(m))]
 
 print("Created processes %s" % pids)
 
@@ -76,11 +91,33 @@ for pid in pids:
 # was this process crashed already?
 did_crash = []
 
+# stopping times for processes
+timestamp_resume = {p: -1 for p in range(n)}
+
 # loop over milliseconds in time
 start_time = 1000 * time()
 while True:
     # current time difference
-    delta = (1000 * time()) - start_time
+    now = 1000 * time()
+    delta = now - start_time
+
+    # loop over processes
+    for p in range(n):
+        # if process is not stopped yet
+        if timestamp_resume[p] == -1:
+            # stopping it rarely
+            if np.random.rand() <= p_stopped:
+                to_sleep = get_t_stopped()
+                print("Pausing process %d for %.1f ms" % (p + 1, to_sleep))
+                os.kill(pids[p], signal.SIGSTOP)
+                timestamp_resume[p] = now + to_sleep
+
+    # resuming processes
+    for p, t in timestamp_resume.items():
+        if t > 0 and now >= t:
+            print("Resuming process %d" % (p + 1))
+            timestamp_resume[p] = -1
+            os.kill(pids[p], signal.SIGCONT)
 
     # checking if need to crash processes
     for i, process in enumerate(crashed_processes):
@@ -102,6 +139,12 @@ while True:
             
     # sleep for one ms
     sleep(1e-3)
+
+# resuming all sleeping processes
+for p in timestamp_resume.keys():
+  if timestamp_resume[p] > 0:
+    print("Resuming process %d" % (p + 1))
+    os.kill(pids[p], signal.SIGCONT)
 
 # waiting for log to be written
 sleep(wait_time)
--- tests/test_fifo.sh
+++ tests/test_fifo.sh
@@ -2,9 +2,11 @@
 
 make -C .. -j4
 
+sudo tc qdisc change dev lo root netem delay 50ms 200ms loss 10% 25% reorder 25% 50%
+
 while true
 do 
-  python run_with_crashes.py .. | tee run.log
+  python run_with_crashes.py ..
   python test_fifo_all_properties.py .. | tee test.log
   if [ "$(cat test.log|grep INCORRECT|wc -l)" != "0" ]
   then
