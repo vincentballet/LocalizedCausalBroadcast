e111f71e3d464a6d65ed8afaedc3b55eaef4b912
Vincent Ballet <vincent.ballet@me.com>
2018-11-01 15:11:33 +0100

    Fixes error in test

--- check_output.sh
+++ check_output.sh
@@ -8,7 +8,7 @@ echo -e "\nTESTING PROCESS OUTPUT.\n"
 while [ -n "$1" ]; do
     filename="da_proc_${1}.out"
     echo "Checking file ${filename}."
-    ./check_fifo.py $filename
+    python check_fifo.py $filename
     if [ $? -ne 0 ]; then
         echo "Error in file ${filename}."
         echo -e "\nTEST FAILED.\n"
