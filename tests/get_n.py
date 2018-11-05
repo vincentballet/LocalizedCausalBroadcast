c779beafd5daab818cf505cd46bb1b694861ecbf
Sergei Volodin <sergei.volodin@epfl.ch>
2018-11-05 22:04:11 +0100

    Add get_n

---
+++ tests/get_n.py
@@ -0,0 +1,24 @@
+import numpy as np
+from tqdm import tqdm
+import pickle
+from get_time import get_time
+
+# repetitions
+repetitions = range(5)
+
+# n processes
+ns = list(range(2, 7))
+
+# messages
+m = 500
+
+# results: map n proc -> array over repetitions
+results = {n: [] for n in ns}
+
+# doing the computations
+for n in ns:
+    for _ in tqdm(repetitions):
+        results[n].append(get_time(m, n))
+
+# saving data
+open('results_n.pkl', 'wb').write(pickle.dumps(results))
