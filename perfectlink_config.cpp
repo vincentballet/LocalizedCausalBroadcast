6979b5726d3ba9df084a32d2763d5da43ed320af
Vincent Ballet <vincent.ballet@me.com>
2018-11-06 17:14:24 +0100

    Updating hyperparameters

--- perfectlink_config.cpp
+++ perfectlink_config.cpp
@@ -1,4 +1,4 @@
 #include "perfectlink.h"
 
-const unsigned PerfectLink::TIMEOUT_MSG = 1000;
-const unsigned PerfectLink::MAX_IN_QUEUE = 50;
+const unsigned PerfectLink::TIMEOUT_MSG = 250;
+const unsigned PerfectLink::MAX_IN_QUEUE = 100;
