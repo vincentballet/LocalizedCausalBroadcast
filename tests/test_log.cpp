d0d7a92808f481e714028ef6e2c3ad121b75a2b0
Sergei Volodin <sergei.volodin@epfl.ch>
2018-12-13 23:20:43 +0100

    Add test_log

---
+++ tests/test_log.cpp
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <cassert>
+
+int main()
+{
+  // number of threads
+  int n;
+
+  // number of messages
+  int m;
+
+  // reading first line
+  assert(scanf("%d %d\n", &n, &m) == 2);
+
+  // sequence numbers for each thread
+  int* seqs = new int[n];
+
+  // zeroing sequence numbers
+  for(int i = 0; i < n; i++)
+    seqs[i] = 0;
+
+  // reading the rest
+  for(int i = 0; i < n * m; i++)
+  {
+    // thread id
+    int t;
+
+    // message id
+    int msg;
+
+    // reading a message
+    assert(scanf("thread %d message %d\n", &t, &msg) == 2);
+
+    // sanity check
+    assert(t < n);
+
+    // checking that the next message is correct
+    assert(seqs[t] == msg);
+
+    // incrementing corresponding sequence number
+    seqs[t]++;
+  }
+
+  printf("CORRECT\n");
+}
--- tests/test_log.sh
+++ tests/test_log.sh
@@ -3,10 +3,12 @@
 cd ..
 make -j10
 
+g++ -O3 -o tests/test_log tests/test_log.cpp
+
 while true
 do
   ./da_proc 1 ./membership testLOG
-  python tests/test_log.py 2>&1 | tee tests/output.txt
+  cat da_proc_1.out | ./tests/test_log
   if [ "X$?" != "X0" ]
   then
     echo "Error, please see output.txt"
